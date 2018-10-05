98ae25ed3631c0c9b2d852e2cae473fbfe2828bf
fantypants <8563615@gmail.com>
2018-10-05 12:22:36 -0700

    Initial Test scope created

--- test/my_bot_test.exs
+++ test/my_bot_test.exs
@@ -5,4 +5,42 @@ defmodule MyBotTest do
   test "greets the world" do
     assert MyBot.hello() == :world
   end
+
+  test "ets Table Creates and operates correctly" do
+  end
+
+  test "Placement works anywhere on board" do
+  end
+
+  test "Can Turn Left From All Directions" do
+  end
+
+  test "Can Turn Right From All Directions" do
+  end
+
+  test "Toy can move Forwards" do
+  end
+
+  test "Toy can report last position" do
+  end
+
+  test "Game Runs A Simulation 1" do
+  end
+
+  test "Game Runs A Simulation 2" do
+  end
+
+  test "Game Runs A Simulation 3" do
+  end
+
+  test "Game Runs A Simulation 4" do
+  end
+
+  test "Incorrect Coordinates are caught" do
+  end
+
+  test "Incorrect Direction is caught" do
+  end
+
+
 end
